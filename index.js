eb682f014530663f73e28239c5dfefe56b25f146
Manojchinnaiyan <manojchinnaiyan111@gmail.com>
2022-01-31 10:57:27 +0530

    changes

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use( '/', ( req, res ) =>
     res.send("Hello to API")
 })
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 3000
 
 mongoose.connect( process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true } )
     .then( () => app.listen( PORT, () => console.log( `Server is running on port :${ PORT }` ) ) )
