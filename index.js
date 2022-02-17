c03aaba6e2585d9a9ce44544b9cbd43892f517f0
Manojchinnaiyan <manojchinnaiyan111@gmail.com>
2022-02-17 21:56:05 +0530

    server api

--- index.js
+++ index.js
@@ -1,28 +1,31 @@
-import express from 'express'
-import bodyParser from 'body-parser'
-import mongoose from 'mongoose'
-import cors from 'cors'
-import postRouters from './routes/posts.js'
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import userRoutes from "./routes/users.js";
+import postRoutes from "./routes/posts.js";
 
-const app = express()
-app.use( bodyParser.json( { limit: "30mb", extended: true } ) )
-app.use( bodyParser.urlencoded( { limit: "30mb", extended: true } ) )
-app.use( cors() )
+const app = express();
+dotenv.config();
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
 
-app.use( '/post', postRouters )
-app.use( '/', ( req, res ) =>
-{
-    res.send("Hello to API")
-})
+app.use("/post", postRoutes);
+app.use("/user", userRoutes);
+app.use("/", (req, res) => {
+  res.send("Hello to API");
+});
 
-const port = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
-mongoose.connect( process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true } )
-    .then( () => app.listen( port, () => console.log( `Server is running on port :${ PORT }` ) ) )
-    .catch( ( error ) => console.log( error.message ) )
-    
-
-
-
-
-
+mongoose
+  .connect(process.env.CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server is running on port :${PORT}`))
+  )
+  .catch((error) => console.log(error.message));
--- routes/posts.js
+++ routes/posts.js
@@ -1,11 +1,22 @@
-import express from 'express'
-import { getPost,createPost,updatePost,deletePost,likePost } from '../controllers/posts.js'
-const router = express.Router()
+import express from "express";
+import {
+  getPosts,
+  getPost,
+  getPostBySearch,
+  createPost,
+  updatePost,
+  deletePost,
+  likePost,
+} from "../controllers/posts.js";
+const router = express.Router();
+import auth from "../middleware/auth.js";
 
-router.get( '/', getPost )
-router.post( '/', createPost )
-router.patch( '/:id', updatePost )
-router.delete( '/:id', deletePost )
-router.patch('/:id/likePost', likePost)
+router.get("/search", getPostBySearch);
+router.get("/", getPosts);
+router.get("/:id", getPost);
+router.post("/", auth, createPost);
+router.patch("/:id", updatePost);
+router.delete("/:id", auth, deletePost);
+router.patch("/:id/likePost", auth, likePost);
 
-export default router
+export default router;
